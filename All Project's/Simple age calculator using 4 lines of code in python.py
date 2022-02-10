8d35d7ea079803fe0f0807f4d9cc587242bb1a90
SIyam Sorkar <rksiyam479.gmail.com>
2022-02-11 03:51:29 +0600

    A simple age calculator created with 4 lines of code

---
+++ All Project's/Simple age calculator using 4 lines of code in python.py
@@ -0,0 +1,6 @@
+                            #The simple one to calculate only the year
+
+age  = 2022
+birth_year = int(input("Enter your birth year: "))
+age -= (birth_year)     #Augmented assignment operator.
+print(f"Your are currently {age} years old")   #Formatted string to simplify the code.
